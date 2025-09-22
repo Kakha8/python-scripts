40ceba72554bfa766374439d8323bda148557aa7
Kakha8 <kakha.kudava.98@gmail.com>
2025-09-22 03:33:38 +0300

    Added spec info

--- System-Monitor-Flask/SysMon.py
+++ System-Monitor-Flask/SysMon.py
@@ -1,9 +1,42 @@
-from flask import Flask, Response, render_template
+from flask import Flask, Response, render_template, jsonify
 import psutil
+import platform
 import time
 import json
+import cpuinfo
+import wmi
+
+
 
 app = Flask(__name__)
+
+def getCpuSpecs():
+    cpu_spec = cpuinfo.get_cpu_info()
+
+    architechture = cpu_spec['arch']
+    cpu_general = cpu_spec['brand_raw']
+    clock_speed = cpu_spec['hz_advertised_friendly']
+
+    cpu_info = {
+        "CPU": cpu_general,
+        "Architecture": architechture,
+        "Cores": psutil.cpu_count(logical=False),
+        "Clock_speed": clock_speed
+    }
+    #print(cpu_spec['brand_raw'])
+    print(cpu_spec)
+
+    return cpu_info
+
+def getRamSpecs():
+    ram_total = psutil.virtual_memory().total / (1024 ** 3)
+    ram_total = round(ram_total, 1)
+    return {"ram_total": ram_total}
+
+def getDiskSpecs():
+    disk_total = psutil.disk_usage("C:\\").total / (1024 ** 3)
+    disk_total = round(disk_total, 1)
+    return {"disk_total": disk_total}
 
 def stream_usage():
     psutil.cpu_percent(interval=None)  # warm up
@@ -27,8 +60,8 @@ def stream_usage():
 def index():
     return render_template("index.html")
 
-@app.route("/stream")
-def stream():
+@app.route("/live_usage")
+def liveUsage():
     return Response(
         stream_usage(),
         mimetype="text/event-stream",
@@ -38,5 +71,14 @@ def stream():
         },
     )
 
+@app.route("/specs")
+def specs():
+    return jsonify({
+        "cpu": getCpuSpecs(),
+        "ram": getRamSpecs(),
+        "disk": getDiskSpecs()
+    })
+
 if __name__ == "__main__":
+    #getCpuSpecs()
     app.run(host="127.0.0.1", port=5000, debug=True, threaded=True, use_reloader=False)
